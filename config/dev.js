b606238ae08560dec036522fef432963bad4357f
김재준 <kimsman@gimjaejuns-MacBook-Pro.local>
2019-12-15 17:48:45 +0900

    Security Setting

---
+++ config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+   mongoURI : 'mongodb+srv://kimsman:Kjj030619!@@react-blog-9hojk.mongodb.net/test?retryWrites=true&w=majority'
+}
--- index.js
+++ index.js
@@ -1,17 +1,37 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://kimsman:Kjj030619!@@react-blog-9hojk.mongodb.net/test?retryWrites=true&w=majority',
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const config = require('./config/key')
+
+const { User } = require('./models/user');
+
+mongoose.connect(config.mongoURI,
     {useNewUrlParser: true }).then(() =>console.log('DB connected'))
                              .catch(err =>console.error(err));
 
 
+app.use(bodyParser.urlencoded({ extended : true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
 
-app.get('/', (req,res)=>{
-    res.send('hello world')
-});
+app.get('/',(req,res)=>{
+    res.json({"hello~": "Hi ~ dfadfadf"})
+})
 
+app.post('/api/users/resgister',(req,res) => {
+        const user = new User(req.body)
 
+        user.save((err, userData) => {
+            if(err) return res.json ({ success: false, err })
+        })
+        return res.status(200).json({
+            success:true
+        })
+
+})
 
 
 
